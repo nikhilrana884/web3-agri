201b3a739787829984f8f24ea7ef487116a31721
Nikhil Rana <nikhilrana884@gmail.com>
2023-02-07 14:29:55 +0530

    Update marketplace.sol

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -9,8 +9,8 @@ import "hardhat/console.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private tokenIds;
-    Counters.Counter private itemsSold;
+    Counters.Counter private tokenIds_;
+    Counters.Counter private itemsSold_;
 
     uint256 listingPrice = 0.0001 ether;
     address payable owner;
@@ -21,9 +21,9 @@ contract NFTMarketplace is ERC721URIStorage {
         uint256 tokenId;
         address payable seller;
         address payable owner;
-        uint256 quantity;
+        //uint256 quantity;
         uint256 price;
-        string category;
+        //string category;
         bool sold;
     }
 
@@ -31,10 +31,8 @@ contract NFTMarketplace is ERC721URIStorage {
         uint256 indexed tokenId,
         address seller,
         address owner,
-        uint256 quantity,
         uint256 price,
-        string ctaegory,
-        bool sold,
+        bool sold
     );
 
     modifier onlyOwner() {
@@ -47,6 +45,64 @@ contract NFTMarketplace is ERC721URIStorage {
 
     constructor() ERC721("AgriMarket Token", "AGR3") {
         owner = payable(msg.sender);
+    }
+    
+    function updateListingPrice(uint256 listingPrice_)
+        public
+        payable
+        onlyOwner
+    {
+        require(
+            owner == msg.sender,
+            "Only marketplace owners can update listing price."
+        );
+        listingPrice = listingPrice_;
+    }
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createToken(string memory tokenURI, uint256 price)
+        public
+        payable
+        returns (uint256)
+    {
+        tokenIds_.increment();
+        uint256 newTokenId = tokenIds_.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        createMarketItem(newTokenId, price);
+        //setIemQuantity(newTokenId, quantity);
+        //setItemCategory(newTokenId, category);
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        require(price > 0, "Price must be at least 1");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to the listing price."
+        );
+
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+        
+        emit MarketItemCreated(
+            tokenId,
+            msg.sender,
+            address(this),
+            price,
+            false
+        );
     }
 
 }
